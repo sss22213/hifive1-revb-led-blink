0518e3c682cfb04b568d27692c6f2f546d50f091
sss22213 <n0404.n0404@gmail.com>
2020-11-07 10:27:48 -0800

    fix

--- blink.c
+++ blink.c
@@ -7,18 +7,18 @@ int main() {
     struct metal_cpu *cpu = metal_cpu_get(0);
     struct metal_interrupt *cpu_int = metal_cpu_interrupt_controller(cpu);
     metal_interrupt_init(cpu_int);
-    // set led pin
+	// set led pin
 	led0_blue = metal_led_get_rgb("LD0", "blue");
-    // enable led
+	// enable led
 	metal_led_enable(led0_blue);
-    // off led
+	// off led
 	metal_led_on(led0_blue);
-    while(1)
-    {
-        DELAY;
+	while(1)
+	{
+		DELAY;
 		metal_led_off(led0_blue);
-        DELAY;
-        metal_led_on(led0_blue);
-    }
+		DELAY;
+		metal_led_on(led0_blue);
+	}
     return 0;
 }
